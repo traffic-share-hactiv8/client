2f42a1e512c89306e6bc74db62cd2d38c85e61e3
yasirmaulana <yasir.maulana@gmail.com>
2018-06-07 20:00:20 +0700

    add fitur login - client; add router login

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import cRegister from '@/components/com-register.vue'
+import cLogin from '@/components/com-login.vue'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: cRegister
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: cLogin
     }
   ]
 })
